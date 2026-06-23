eb698396e66f2868ebc12c35e4f7819cc1520cd1
Said-bourhabi <abousaid11araqi@gmail.com>
2026-06-23 21:44:29 +0100

    feat (verecel): add custom vercel analytics

--- qr-boot/src/main.jsx
+++ qr-boot/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { HelmetProvider } from 'react-helmet-async'
+import { Analytics } from "@vercel/analytics/react"
 import './index.css'
 import App from './App.jsx'
 
@@ -9,6 +10,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HelmetProvider>
       <BrowserRouter >
+        <Analytics />
         <App />
       </BrowserRouter>
     </HelmetProvider>
